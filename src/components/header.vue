f7085966e18f832fbe2862014fdf5f506c252804
Mvelasto <milbelis@gmail.com>
2022-01-21 20:00:50 -0600

    añadiendo componentes

---
+++ src/components/header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="m-0 p-0">
+        <div class="p-5 bg-dark text-light">
+            <h1>header vue <i class="fas fa-address-card"></i></h1>
+        </div>
+    </div>
+    
+</template>
