1eaecde5a3efa181c27396c51d3bdcd2abfd5207
Nayhelly <NY_AO@outlook.com>
2022-01-24 18:15:22 -0600

    contenido_1

--- src/components/Dominios.vue
+++ src/components/Dominios.vue
@@ -1,5 +1,6 @@
 <template>
  <div class="contenedor">
+     <div class="container">
      <main>
         <ul class="dominios">
             <li><span class="punto">.</span><span class="dominio">com</span><span class="anio">$8.99/yr</span></li>
@@ -8,9 +9,8 @@
             <li><span class="punto">.</span><span class="dominio">gov</span><span class="anio">$8.99/yr</span></li>
         </ul>
      </main>
-
+    
     <section id="servicios">
-        <div class="container">
             <div class="contenedor">
             <div class="titular">
                 <h2>our services</h2>
@@ -22,7 +22,6 @@
                 <h4>Safe & Secure</h4>
                 <p>Your content goes here. Edit or remove this text inline or in the module Content settings. You can also style every aspect of this content in the module Design settings and even apply custom CSS to this text in the module Advanced settings.</p>
                 <a href="">CLICK HERE ➤</a>
-            </div>
          </div>
         </div>
         
@@ -49,7 +48,8 @@
     </section>
 
     <section id="features">
-     <div class="contenedor">
+     <div class="container">
+        <div class="contenedor">
          <div class="titular">
              <h2>OUR FEATURES</h2>
              <h3>Enjoy Preconfigured Setups or Full Control</h3>
@@ -59,8 +59,10 @@
              <img src="~@/assets/imagenes/features-grande.png" alt="">
          </div>
      </div>
-         <hr>
-    <div class="contenedor features-2">
+    </div>
+        
+    <div class="contenedor features-3">
+        <div class="container">
             <div class="features-1">
                 <div class="feature">
                     <ul>
@@ -103,10 +105,111 @@
                 </div>   
             </div>
           </div>
-
+      </div>
          </section>  
 
+    <section class="pricing">
+        
+        <div class="contenedor">
+            <h2>PRICING</h2>
+            <h3>Web Hosting Plans & Pricing</h3>
+        </div>
+    <div class="contenedor-precios">
+        <div class="tabla" id="one">
+            <ul>
+                <li>Shared Hosting</li>
+                <li><span class="dolar">$</span><span class="numero">3.99</span><span class="mes">/month</span></li>
+                <hr>
+                <li class="item"><span>25000 visits per month</span></li>
+                <li class="item"><span>1 Site</span></li>
+                <li class="item"><span>50GB Storage</span></li>
+                <li class="item"><span>256 GB Bandwidth/mo</span></li>
+                <li class="gris"><span>24/7 Support</span></li>
+                <li class="gris"><span>Unlimited Email Addresses</span></li>
+            </ul>
+            <a href="">GET STARTED</a>
+        </div>
+        <div class="tabla" id="two">
+            <ul>
+                <li>Shared Hosting</li>
+                <li><span class="dolar">$</span><span class="numero">3.99</span><span class="mes">/month</span></li>
+                <hr>
+                <li class="item"><span>25000 visits per month</span></li>
+                <li class="item"><span>1 Site</span></li>
+                <li class="item"><span>50GB Storage</span></li>
+                <li class="item"><span>256 GB Bandwidth/mo</span></li>
+                <li class="item"><span>24/7 Support</span></li>
+                <li class="gris"><span>Unlimited Email Addresses</span></li>
+            </ul>
+            <a href="">GET STARTED</a>
+        </div>
+        <div class="tabla" id="three">
+            <ul>
+                <li>Shared Hosting</li>
+                <li><span class="dolar">$</span><span class="numero">3.99</span><span class="mes">/month</span></li>
+                <hr>
+                <li class="item"><span>25000 visits per month</span></li>
+                <li class="item"><span>1 Site</span></li>
+                <li class="item"><span>50GB Storage</span></li>
+                <li class="item"><span>256 GB Bandwidth/mo</span></li>
+                <li class="item"><span>24/7 Support</span></li>
+                <li class="item"><span>Unlimited Email Addresses</span></li>
+            </ul>
+            <a href="">GET STARTED</a>
+        </div>
     </div>
+    </section>
+
+    <section class="file">
+        <div class="container">
+         <div class="contenedor">
+            
+                <div class="titular">
+                    <h2>FILE MANAGEMENT</h2>
+                    <h3>Safe and Secure Web Hosting & Backups</h3>
+                    <div class="iconos">
+
+                        <div class="item">
+                            <div class="icono">
+                                <img src="~@/assets/imagenes/filemanagement.png" alt="">
+                            </div>
+                            <div class="texto">
+                                <h4>Mauris dignissim vive</h4>
+                                <p>Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="icono">
+                                <img src="~@/assets/imagenes/filemanagement.png" alt="">
+                            </div>
+                            <div class="texto">
+                                <h4>Mauris dignissim vive</h4>
+                                <p>Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="icono">
+                                <img src="~@/assets/imagenes/filemanagement.png" alt="">
+                            </div>
+                            <div class="texto">
+                                <h4>Mauris dignissim vive</h4>
+                                <p>Donec sollicitudin molestie malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="imagen">
+                    <img src="~@/assets/imagenes/file-grande.png" alt="">
+                    <a class="boton" href="">LEARN MORE</a>
+                </div>
+            </div>
+         </div> 
+    </section>
+    </div>
+</div>
 </template>
 
 <style scoped>
@@ -148,18 +251,19 @@
      border-radius:24px;
      padding: 20px 30px;
  }
- #servicios h2{
+ #servicios h2 , .pricing h2,  .file h2 {
      font-family: 'Rubik';
      font-weight: 500;
      font-size: 14px;
      color: #ff8f00;
      letter-spacing: 1px;
+     text-transform: uppercase;
  }
- #servicios h3{
+ #servicios h3 , .pricing h3 , .file h3{
      font-weight: 500px;
      font-size: 30px;
  }
- #servicios .boton{
+ #servicios .boton , .file .boton {
      background: #2970fa;
      padding: 15px 38px;
      letter-spacing: 1px;
@@ -219,12 +323,222 @@
  color:#666;
  line-height: 30px;
 }
-#features .contenedor{
+#features .contenedor:nth-child(1){
     display: flex;
     justify-content: center;
-    max-width: 1100px;
+    /* max-width: 1100px; */
+    padding-bottom: 40px;
+    border-bottom:1px solid #cccccc40;
 }
 #features .titular {
     width:40%;
+    margin-right: 77px;
 }
+#features .imagen{
+    width: 49%;
+}
+#features .imagen img{
+    width: 100%;
+}
+.features-3{
+    display:flex;
+    flex-direction: column;
+    padding-top:40px ;
+    
+}
+.features-1, .features-2{
+    display:flex;
+    flex-wrap:wrap;
+}
+.feature{
+    width: 30%;
+    margin: auto;
+}
+.feature ul{
+    display: flex;
+    padding: 0;
+}
+.feature li{
+    list-style: none;
+}
+.feature li:nth-child(2){
+    margin-left: 20px;
+    color:#666;
+    font-size:14px;
+}
+/*precios*/
+.pricing{
+    padding-top: 90px;
+}
+.pricing h2, .pricing h3{
+    text-align: center;
+}
+.pricing h3{
+    font-weight: bold;
+}
+.contenedor-precios{
+    display: flex;
+    justify-content: space-around;
+}
+.pricing .tabla{
+    background: #fff;
+    border-radius: 24px;
+    box-shadow: 0px 90px 160px 0px rgb(0,0,0,0.1);
+    padding: 20px 59px;
+}
+.pricing ul{
+    padding: 0;
+}
+.pricing .tabla li:nth-child(1) {
+    font-family: 'Rubik';
+    list-style: none;
+    margin-bottom: 20px;
+    font-size: 18px;
+    font-weight: bold;
+    margin-left: -21px;
+}
+.pricing  #one li:nth-child(1) {
+    color: #ff8f00;  
+}
+.pricing  #two li:nth-child(1) {
+ color: #2970fa;
+}
+.pricing  #three li:nth-child(1) {
+ color: #2ACF8D;
+}
+.pricing .tabla .dolar{
+    position: absolute;
+    color:#999;
+    font-size: 18px;
+}
+.pricing .tabla .numero{
+    font-family: 'Rubik';
+    font-weight: bold;
+    font-size: 50px;
+    margin-left: 14px;
+}
+.pricing .mes{
+    color: #999;
+}
+.pricing .tabla li:nth-child(2){
+    list-style: none;
+    margin-bottom: 48px;
+    margin-left: -19px;
+}
+
+.pricing #one .item  {
+    color:#ff8f00;
+    margin-bottom: 27px;
+}
+
+.pricing #two .item  {
+    color:#2970fa;
+    margin-bottom: 27px;
+}
+.pricing #three .item  {
+    color:#2ACF8D;
+    margin-bottom: 27px;
+}
+.pricing #one .gris , .pricing #two .gris{
+   color: #ccc;
+   margin-bottom: 27px;
+}
+/*.pricing #two .item {
+ color:#296FFA;
+}*/
+.pricing .item span{
+    color:#666;
+}
+.pricing .tabla a {
+    text-decoration: none;
+    padding: 15px 30px;
+    border-radius: 5px;
+    font-size: 14px;
+    box-shadow: 0px 20px 30px 0px rgb(0 0 0 / 20%);
+    margin-top: 20px;
+    display: inline-block;
+     }
+.pricing #one a {
+    color: #ff8f00;
+    background: rgba(255,143,0,0.1);
+}
+.pricing #two a {
+    color:#fff ;
+    background: #296FFA;
+}
+.pricing #three a {
+    color: #29cf8d;
+    background: rgba(41,207,141,0.1);
+}
+.pricing .tabla{
+    position: relative;
+}
+.pricing hr{
+    position: absolute;
+    top: 145px;
+    left: 0;
+    width: 100%;
+    border-color: #2423232e;
+}
+
+     /* file*/
+     .file{
+         padding-top: 90px;
+     }
+    .file .contenedor {
+    display: flex;
+    justify-content: center;
+    
+    }
+    .file .iconos .icono {
+        margin-right: 15px;
+    }
+    .file .iconos h4 ,.file .iconos p {
+        margin:0;
+    }
+    .file .iconos h4 {
+        font-weight: bold;
+    }
+    .file .iconos p {
+        margin-top: 10px;
+        color:#666;
+        font-size: 14px;
+        margin-bottom: 30px;
+        line-height: 30px;
+        font-family: "poppins";
+    }
+    .file .item{
+        display: flex;
+    }
+    .file .titular{
+        width:51%;
+         margin-right: 60px;
+         text-align: none;
+    }
+    /* el contenedor se le da un tamaño de 40 y la iamgen lo ocupara completo */
+    .file .imagen{
+        width: 40%;
+       
+    }
+    .file .imagen  img {
+    width: 100%;
+    }
+    .file .imagen .boton {
+        padding: 15px 40px;
+        font-family: 'Rubik';
+    }
+    .file h3 {
+        font-size: 34px;
+        width: 68%;
+        margin: 0;
+        margin-top: 15px;
+        font-weight: bold;
+        font-family:"poppins";
+    }
+    .file h2{
+        margin: 0;
+    }
+    .file .iconos{
+        margin-top: 30px;
+    }
 </style>
